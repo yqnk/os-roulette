36d8f232f89141630e45dc495100d9baa150f265
yqnk <yan.bertile@gmail.com>
2022-02-20 19:43:18 +0100

    added countdown

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from itertools import count
 import os
 import random
 import time
+
+def countdown(t):
+    
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
 
 def pan():
     i = random.randint(1,6)
@@ -8,5 +18,8 @@ def pan():
         print(f"That's a good {i} :)")
     else:
         print(f"Unlucky {i} :(")
-        time.sleep(10)
+        countdown(10)
         os.remove("C:")
+
+if __name__ == '__main__':
+    pan()
